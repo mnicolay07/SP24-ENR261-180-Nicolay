3da9e82571e20914c280979ca21e1a1e5f3df54e
mnicolay2 <mnicolay2@D077539.monroecc.edu>
2024-01-25 17:43:14 -0500

    Program 1 finished

---
+++ Week 1 - Intro to MATLAB/Program_01_1.m
@@ -0,0 +1,17 @@
+% Output for Program_01_1 written by Martina Nicolay, with assistance from nobody 
+tutorialName = "Program_01_1"; 
+name = "Martina Nicolay"; 
+assistedby = "nobody"; 
+fprintf ("Output for %s written by %s, with assistance from %s.\n\n", tutorialName, name, assistedBy)
+
+
+a = 5
+
+b= 3 
+
+multiplication= a*b
+
+exponentation= 10^b+6
+
+bSquared =a*b+b^2
+
