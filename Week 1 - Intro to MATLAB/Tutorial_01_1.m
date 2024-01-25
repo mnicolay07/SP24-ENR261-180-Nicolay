2cf790feddf68c67196a9cb939ec7ae13817ced7
mnicolay2 <mnicolay2@D077539.monroecc.edu>
2024-01-25 17:52:05 -0500

    Finished Tutorial 1

---
+++ Week 1 - Intro to MATLAB/Tutorial_01_1.m
@@ -0,0 +1,25 @@
+% Program Description:
+% The purpose of this program is to demonstrate common arithmetic
+% operations in MATLAB.
+
+% Clear the command window and all variables
+clc  % clc clears the contents of the command window
+clear  % clear clears the command window and all variables
+
+% Output of the title and author to the command window.
+tutorialName = "Tutorial_01_1";
+name = "Martina Nicolay";
+assistedBy = "Nobody";
+fprintf("Output for %s written by %s, with assistance from %s.\n\n", tutorialName, name, assistedBy)
+
+% Finish the following commands
+% Add 2 and 3
+addition = 2+3 
+% Subtract 3 from 2
+subtraction =2-3 
+% Multiply 2 by 3
+multiplication = 2*3
+% Divide 1 by 2
+division =1/2
+% Show 2 to the power of 3
+exponentiation = 2^3
