9614628eb8c4d0f55f1fcd492a2bce10c25dda5e
mnicolay2 <mnicolay2@D077571.monroecc.edu>
2024-02-29 17:19:42 -0500

    Program 3-1 HELP

---
+++ Week 3 - Input Output & Control Structures/Program_03_1.m
@@ -0,0 +1,46 @@
+% Program Description:
+% The purpose of this program is to ...
+
+% Clear the command window and all variables
+clc     % Clear the command window contents
+clear   % Clear the workspace variables
+
+% Output of the title and author to the command window.
+programName = "Program_03_1";
+name = "Martina Nicolay";
+assistedBy = "Cansu T. and Hannah R.";
+fprintf("Output for %s written by %s, with assistance from %s.\n\n", programName, name, assistedBy)
+
+%Current month and current year 
+curr_month= 2;
+curr_year= 2024; 
+
+%Prompt for years 
+year_prompt= "What is your birth year?\n";
+year_ans= input(year_prompt); 
+
+%Prompt for months 
+month_prompt = "What is your birth month?\n";
+month_str = input(month_prompt, 's'); %string
+month_ans= str2double(month_str); %convert to double
+
+%user months and total months 
+user_months =(year_ans*12)+ month_ans;
+total_years = (curr_year -year_ans);
+total_months= total_years*12 +curr_month;
+
+%month statement 
+if month_ans > curr_month
+    total_years = total_years -1; 
+else 
+    total_years = total_years;
+end 
+
+%overall total 
+if total_months> 0 && total_months < 240 
+    fprintf("%d? Wow you're pretty young!\n", total_years);
+elseif total_months >= 240 && total_months <= 480 
+    fprintf("%d?You're getting up there.\n",total_years);
+elseif total_months >=480
+    fprintf("%d? You're pretty old!.\n", total_years);
+end
